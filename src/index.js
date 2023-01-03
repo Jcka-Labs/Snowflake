3f8dd538048ce696709a575289f7dcdbd3478d13
rpc404 <theboringschool.org@gmail.com>
2023-01-03 13:00:58 +0545

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css";
+
+export default function SnowFlake() {
+  return (
+    <div className="snowflakes" aria-hidden="true">
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+    </div>
+  );
+}
---
+++ src/test.js
@@ -0,0 +1,7 @@
+import SnowFlake from '.'
+
+describe('SnowFlake', () => {
+  it('is truthy', () => {
+    expect(SnowFlake).toBeTruthy()
+  })
+})
